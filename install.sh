2040e2f969b8c70bb08f23a0c55b28ea891f862e
demondamz <74813656+demondamz@users.noreply.github.com>
2022-02-22 20:30:50 +0000

    formatting
    return CLI to default colors and added formatting

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # https://sudobox.io
 #
 clear
-function installation() {
+function installation () {
     echo -e "\e[34m\e[1m--------------- Sudobox Installer ---------------"
     echo "  Pre Installer for SudoBox.io "
     echo "  Version 0.0.1 "
@@ -28,7 +28,7 @@ function installation() {
     esac
 }
 
-function checkIfSudo() {
+function checkIfSudo () {
     if [ "$EUID" -ne 0 ]; then
         echo -e "\e[91mError, Please run as root, exiting...."
         exit
@@ -41,12 +41,12 @@ function checkIfSudo() {
         installsbbackend
         installsbcli
         echo "Finished installing all Dependencies, Moving to CLI Questions..."
-        echo "You can run SudoBox CLI at anytime using 'sb'"
+        echo -e "\e[39mYou can run SudoBox CLI at anytime using 'sb'"
     fi
 
 }
 
-function downloadDependencies() {
+function downloadDependencies () {
     echo -e "\e[34m\e[1mInstalling and ensuring your system is upto date"
     sudo apt-get -qq update -y && sudo apt-get -qq upgrade -y
     echo -e "\e[34m\e[1mProceeding with installation dependencies..."
@@ -70,14 +70,14 @@ function downloadDependencies() {
     fi
 }
 
-function createdir() {
+function createdir () {
     mkdir -p /opt/sudobox/configs
     mkdir -p /opt/sudobox/appdata
     mkdir -p /opt/sudobox/compose
     echo -e "\e[34m\e[1mCreated SudoBox Directories"
 
 }
-function dockernetworkcheckpublic() {
+function dockernetworkcheckpublic () {
     donetcheck=$(docker network ls | grep --only-matching sudobox | head -1)
     if [[ "$donetcheck" == "sudobox" ]]; then
         echo -e "\e[32mDocker Network ( sudobox )Exists, Skipping..."
@@ -87,7 +87,7 @@ function dockernetworkcheckpublic() {
         echo -e "\e[32mCreated Docker Network sudobox"
     fi
 }
-function dockernetworkcheck() {
+function dockernetworkcheck () {
     donetcheck=$(docker network ls | grep --only-matching sudobox_private)
     if [[ "$donetcheck" == "sudobox_private" ]]; then
         echo -e "\e[32mDocker Network ( sudobox_private )Exists, Skipping..."
@@ -97,7 +97,7 @@ function dockernetworkcheck() {
         echo -e "\e[32mCreated Docker Network sudobox_private"
     fi
 }
-function installsbcli() {
+function installsbcli () {
     alias sb='docker run -it --rm --network=sudobox_private --name sb-cli ghcr.io/sudobox-io/sb-cli'
     echo "alias sb='docker run -it --network=sudobox_private --rm --name sb-cli ghcr.io/sudobox-io/sb-cli && clear'" >>~/.bash_aliases
     echo -e "\e[34m\e[1mCreating SudoBox CLI Container"
@@ -105,7 +105,7 @@ function installsbcli() {
     echo -e "\e[32mCreated SudoBox CLI Container"
 }
 
-function installsbbackend() {
+function installsbbackend () {
     cd /opt/sudobox/compose
     echo 'version: "3.5"
 services:
@@ -136,6 +136,7 @@ networks:
     external: true' >sb-backend.yml
     echo -e "\e[32mCreated SudoBox backend compose file"
     docker-compose -f sb-backend.yml up -d
+    echo "Created SudoBox backend Continaer"
 }
 
 installation
